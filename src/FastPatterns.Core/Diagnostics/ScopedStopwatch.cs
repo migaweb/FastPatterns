b84ade48bf82a6f05fa07c0232afd14dbf09fbff
Mikael Storman Gustavsson <miga02@gmail.com>
2025-07-12 15:13:43 +0200

    Added ScopedStopwatch to Core project.

---
+++ src/FastPatterns.Core/Diagnostics/ScopedStopwatch.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace FastPatterns.Core.Diagnostics;
+/// <summary>
+/// A struct that measures the elapsed time of a code block and invokes a callback with the elapsed time when disposed.
+/// </summary>
+public readonly struct ScopedStopwatch(Action<TimeSpan> onDispose) : IDisposable
+{
+  private readonly Stopwatch _stopwatch = Stopwatch.StartNew();
+  private readonly Action<TimeSpan> _onDispose = onDispose;
+
+  /// <summary>
+  /// Stops the stopwatch and invokes the callback with the elapsed time.
+  /// </summary>
+  public void Dispose()
+  {
+    _stopwatch.Stop();
+    _onDispose(_stopwatch.Elapsed);
+  }
+}

c2ed4930626b84e24dd2aa3d7b3aaf757ae552c4
Mikael Storman Gustavsson <miga02@gmail.com>
2025-07-21 13:48:33 +0200

    Update validation example and README

--- FastPatterns.Validation/CreateUserHandler.cs
+++ FastPatterns.Validation/CreateUserHandler.cs
@@ -7,8 +7,7 @@ public record CreateUserCommand : ICommand<bool>
 {
   public string? Name { get; set; }
   public string? Username { get; set; }
-  public string? Password { get; set; }
-  public string? PasswordMatch { get; set; }
+  public string? Email { get; set; }
 }
 
 public class CreateUserCommandValidator : IValidator<CreateUserCommand>
@@ -25,13 +24,13 @@ public class CreateUserCommandValidator : IValidator<CreateUserCommand>
     {
       errors.Add(new ValidationError(nameof(request.Username), "Username is required."));
     }
-    if (string.IsNullOrWhiteSpace(request.Password))
+    if (string.IsNullOrWhiteSpace(request.Email))
     {
-      errors.Add(new ValidationError(nameof(request.Password), "Password is required."));
+      errors.Add(new ValidationError(nameof(request.Email), "Email is required."));
     }
-    if (request.Password != request.PasswordMatch)
+    else if (!request.Email.Contains('@'))
     {
-      errors.Add(new ValidationError(nameof(request.PasswordMatch), "Passwords must match."));
+      errors.Add(new ValidationError(nameof(request.Email), "Email must be valid."));
     }
 
     return Task.FromResult<IEnumerable<ValidationError>>(errors);
